a0414e9102345e43c89b0bb26ef4492c34aa0a96
Ayush Aggarwal <Aama098@gmail.com>
2023-08-07 20:49:56 -0700

    Create MissinNumberArray.py
    https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&sortBy=submissions

---
+++ GFG/Array/MissinNumberArray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def missingNumber(self,arr,n):
+        
+        # for i in range(1,n+1):
+        #     if i not in arr:
+        #         return i
+        
+        # setArr = set()
+        # for num in arr:
+        #     setArr.add(num)
+            
+        # for i in range(1,n+1):
+        #     if i not in setArr:
+        #         return i
+        
+        
+        sumN = n*(n+1)//2
+        sumArr = 0
+        for num in arr:
+            sumArr += num
+            
+        return sumN - sumArr;
